fed6ae1f1e2f846e61416e87aff0f8489b21af5f
Mans hammarstrand <mtn.hammarstrand@gmail.com>
2023-03-29 10:33:40 +0200

    deleted unused providers.dart

--- lib/providers/providers.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_weather_app/models/models.dart';
-import 'package:flutter_weather_app/providers/weather_provider.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import '../services/services.dart';
